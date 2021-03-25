7ba728714a8bb0eeda5f1274a5761ce36ede27d7
MariaGornashevich <lightbrown9874@gmail.com>
2021-03-25 23:59:16 +0300

    Created footer

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
     speed: 1200,
     adaptiveHeight: true,
     prevArrow:
-      '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+      '<button type="button" class="slick-prev"><img src="../icons/chevron left solid.svg"></button>',
     nextArrow:
-      '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+      '<button type="button" class="slick-next"><img src="../icons/chevron right solid.svg"></button>',
     responsive: [
       {
         breakpoint: 992,
